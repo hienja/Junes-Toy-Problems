edf1e608663552b71d7a14be777b6321f07990b9
Hien Tran <aznketchup@yahoo.com>
2016-09-20 16:14:25 -0700

    Add extra credit to movingZeroesToTheEnd problem.

--- movingZeroesToTheEnd/movingZeroesToTheEnd.js
+++ movingZeroesToTheEnd/movingZeroesToTheEnd.js
@@ -3,6 +3,7 @@ Implement a function that takes two arguments, an array and a boolean (default i
 Return the array with all of the zeros to the right if the boolean is true else to the 
 left if the boolean is false, preserving the order of the other elements.
 
+
 Example:
 arrNum = [false,1,0,1,2,0,1,3,"a"]
 moveZeros(arrNum, true) //=> returns[false,1,1,2,1,3,"a",0,0]
@@ -15,4 +16,9 @@ moveZeros(arrNum, false) //=> returns [0, 0, 0, 0, 12, 10, 8, 12, 7, 6, 4, 10, 1
 
 arrNum = [12, 0, 10, 0, 8, 12, 7, 6, 0, 4, 10, 12, 0];
 moveZeros(arrNum) //=> returns [12, 10, 8, 12, 7, 6, 4, 10, 12, 0, 0, 0, 0]
+
+
+Extra Credits:
+1. Implement the function using reduce.
+2. Implement the fucntion using recursion without helper function.
 */
