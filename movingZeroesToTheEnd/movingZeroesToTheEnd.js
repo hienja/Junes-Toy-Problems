edc0bc0322e195f9639aa49bfab3676e2b246cbe
Hien Tran <aznketchup@yahoo.com>
2016-09-20 16:11:48 -0700

    Finished description for movingZeroesToTheEnd problem.

---
+++ movingZeroesToTheEnd/movingZeroesToTheEnd.js
@@ -0,0 +1,18 @@
+/*
+Implement a function that takes two arguments, an array and a boolean (default is true).
+Return the array with all of the zeros to the right if the boolean is true else to the 
+left if the boolean is false, preserving the order of the other elements.
+
+Example:
+arrNum = [false,1,0,1,2,0,1,3,"a"]
+moveZeros(arrNum, true) //=> returns[false,1,1,2,1,3,"a",0,0]
+
+arrNum = [12, 0, 10, 0, 8, 12, 7, 6, 0, 4, 10, 12, 0];
+moveZeros(arrNum, true) //=> returns [12, 10, 8, 12, 7, 6, 4, 10, 12, 0, 0, 0, 0]
+
+arrNum = [12, 0, 10, 0, 8, 12, 7, 6, 0, 4, 10, 12, 0];
+moveZeros(arrNum, false) //=> returns [0, 0, 0, 0, 12, 10, 8, 12, 7, 6, 4, 10, 12]
+
+arrNum = [12, 0, 10, 0, 8, 12, 7, 6, 0, 4, 10, 12, 0];
+moveZeros(arrNum) //=> returns [12, 10, 8, 12, 7, 6, 4, 10, 12, 0, 0, 0, 0]
+*/
